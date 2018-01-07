179b1f8a7a8a0645ad4a9bd60f4f21d465317274
Mallikarjuna Kalkere <mkalkere@gmail.com>
2018-01-07 14:49:02 -0500

    Initial Commit

---
+++ MyFirstEFCoreApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstEFCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
